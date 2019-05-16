e368201bac287e6b2638979c5a7489835a9794b6
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2019-05-15 20:54:01 -0700

    Day One Work - Write Basic Calculations

--- profile.cc
+++ profile.cc
@@ -1,5 +1,28 @@
 #include "profile.h"
 
-const double Profile::GetTime() {
-  return 0.0;
+// NOTE - This is a very rough draft, and not a finished product. These are just some of the calculations I will be using
+const double Profile::GetTime(ProfilePoint current, ProfilePoint goal, bool use_max_velocity) {
+	// Variables
+	double a;
+	double t_one;
+
+	double d_one;
+	double d_two;
+
+	double time_tl;
+	double time_r;
+
+	double t_total;
+
+	// Operations for Part Two
+	t_one - (v_max-v_initial) / a;
+
+	d_one = 0.5 * v_max * t_one;
+	d_two = goal - (v_max * t_one);
+
+	time_tl = (v_max - v_initial) / a_max;
+	time_r = (d_two / v_max)
+
+	t_total = (2 * time_t) + time_r;
+
 }
